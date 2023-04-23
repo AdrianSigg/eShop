593e76c8386b07581ae0aff04a341bc581294803
Adrian Sigg <AdrianSigg@gmx.com>
2023-04-23 17:10:15 -0600

    Se crea toda la página en su primera versión, sin backend, puro front, se creo la pagina de inicio, el carrito, el perfil y se implementó el tema oscuro en la aplicación en la sección de configuración.

--- src/app/pages/configuracion/configuracion.page.ts
+++ src/app/pages/configuracion/configuracion.page.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracion.page.scss'],
 })
 export class ConfiguracionPage implements OnInit {
+  darkMode: boolean = false;
 
+  constructor() {
+    this.checkDarkMode();
+  }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  checkDarkMode() {
+    const element = document.body;
+    const styles = window.getComputedStyle(element);
+    const backgroundColor = styles.getPropertyValue('background-color');
+    const backgroundColorHex = this.rgbToHex(backgroundColor);
+    if (backgroundColorHex === ('#121212'||'#000000')) {
+      this.darkMode = true;
+    }else this.darkMode = false;
+  }
+
+  rgbToHex(rgb: string): string {
+    const [r, g, b] = rgb.replace(/[^\d,]/g, '').split(',');
+    return `#${(+r).toString(16)}${(+g).toString(16)}${(+b).toString(16)}`;
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark');
   }
 }
